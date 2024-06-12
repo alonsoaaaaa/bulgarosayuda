0654d4dfd645752cf7c363cbade863d429c6bc8f
Alonso-pixel <kreitos_05@outlook.com>
2024-06-11 20:49:04 -0600

    added ux

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -139,7 +139,7 @@ export default function KefirCalculator2() {
                 type="number"
                 step="0.1"
                 id="temperature"
-                placeholder="ejemplo: 22"
+                placeholder="ejemplo: 20 (temperatura promedio de México) / 5 temperatura en refri"
                 {...register("temperature", { required: true })}
               />
             </div>
