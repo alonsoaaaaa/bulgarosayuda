91e0dd9877298ad6fb7a70cb83b93af6896ba303
Alonso-pixel <kreitos_05@outlook.com>
2024-06-26 08:57:29 -0600

    added google analytics old way

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-Y31YLG2X4G"
+          async
+        />
         <Script id="google-analytics">
-          {
-            "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5N9HQHS8');"
-          }
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Y31YLG2X4G');
+          `}
         </Script>
         <Script src="https://cdn.amplitude.com/libs/analytics-browser-2.7.4-min.js.gz"></Script>
         <Script src="https://cdn.amplitude.com/libs/plugin-session-replay-browser-1.4.1-min.js.gz"></Script>
