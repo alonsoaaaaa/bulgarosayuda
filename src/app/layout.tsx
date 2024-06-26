ded9a9aaeb7a16d859c79be5e2c5f0d9670f14be
Alonso-pixel <kreitos_05@outlook.com>
2024-06-26 08:25:26 -0600

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script id="google-analytics">
+          {
+            "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5N9HQHS8');"
+          }
+        </Script>
         <Script src="https://cdn.amplitude.com/libs/analytics-browser-2.7.4-min.js.gz"></Script>
         <Script src="https://cdn.amplitude.com/libs/plugin-session-replay-browser-1.4.1-min.js.gz"></Script>
         <Script src="https://cdn.amplitude.com/libs/plugin-autocapture-browser-0.9.0-min.js.gz"></Script>
@@ -30,6 +35,14 @@ export default function RootLayout({
         </Script>
       </head>
       <body className={inter.className + "flex items-center justify-center"}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5N9HQHS8"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
         <Container>{children}</Container>
       </body>
     </html>
