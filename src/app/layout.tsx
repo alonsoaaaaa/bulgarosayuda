66f469cff58f905f5f615dff60b360145987eff4
Alonso-pixel <kreitos_05@outlook.com>
2024-06-26 08:27:24 -0600

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
             src="https://www.googletagmanager.com/ns.html?id=GTM-5N9HQHS8"
             height="0"
             width="0"
-            style="display:none;visibility:hidden"
+            style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
         <Container>{children}</Container>
